5f082e0e41f808460f129c859334dbe440eea575
elena_chilese <elena.chilese@studenti.unipd.it>
2023-06-15 00:40:35 +0200

    ultimo push

--- View/Item/aulaconcerto_item.h
+++ View/Item/aulaconcerto_item.h
@@ -2,12 +2,14 @@
 #define AULACONCERTO_ITEM_H
 
 #include "abstact_aula.h"
+#include <QHBoxLayout>
 
 class aulaConcerto_item : public abstractAula_item{
 private:
     //unsigned int availability;
     //double price;
     //inserire solo gli elementi "in più" rispetto ad aula_abstract
+    QHBoxLayout* hbox;
 public:
     aulaConcerto_item();//inserire i parametri del costruttore tutti
 };
